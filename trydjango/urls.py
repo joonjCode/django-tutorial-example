bf79511edf5cebb2da1ff16a43e3fb0454eb4edc
Joon J <jtj308@naver.com>
2021-12-14 00:56:52 +0900

    Add: Login views and template

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_view
-from articles import views
+from accounts.views import login_view, logout_view, register_view
+from articles.views import article_create_view, article_detail_view, article_search_view
 
 urlpatterns = [
     path("", home_view, name="home"),
-    path("articles/", views.article_search_view),
-    path("articles/create/", views.article_create_view),
-    path("articles/<int:id>", views.article_detail_view),
+    path("login/", login_view),
+    path("articles/", article_search_view),
+    path("articles/create/", article_create_view),
+    path("articles/<int:id>", article_detail_view),
     path("admin/", admin.site.urls),
 ]
